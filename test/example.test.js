1cb767bc23292edd4874f34b52ecde3da9d21333
Estefani <stef45689@gmail.com>
2021-07-16 11:41:00 -0500

    Refractor Attempt

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,61 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add } from '../utils.js';
 
+// using a test library
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add should take in 5 and 3 and return 8', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(5, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('add should take in 23 and -10 and return 13', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 13;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(23, -10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('add should take in 100 and 0 return 100', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 100;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(100, 0);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add should take in "5" and 3 return 8', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 8;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add('5', 3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
